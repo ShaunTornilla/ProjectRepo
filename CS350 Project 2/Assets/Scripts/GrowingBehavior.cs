3c956285743b4bfb44c2df31aa185f6a30b770b9
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-01 13:27:09 -0500

    Tree grown

---
+++ CS350 Project 2/Assets/Scripts/GrowingBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingBehavior : MonoBehaviour
+{
+
+    public bool grown = false;
+    [SerializeField] Sprite grownSprite;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!grown && collision.gameObject.CompareTag("Player"))
+        {
+            grown = true;
+            GetComponent<SpriteRenderer>().sprite = grownSprite;
+        }
+    }
+}
