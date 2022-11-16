ab8d108cba0780d3eb2cfb277689be87d976cfe6
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-16 17:45:31 -0600

    Tutorial Finished

---
+++ CS350 Project 2/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject beginning;
+    [SerializeField] GameObject worldSpace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (beginning.activeInHierarchy)
+            Time.timeScale = 0f;
+    }
+
+    void OnContinue()
+    {
+        if(beginning.activeInHierarchy)
+        {
+            Time.timeScale = 1f;
+            worldSpace.SetActive(true);
+            beginning.SetActive(false);
+        }
+    }
+}
