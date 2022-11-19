fb86e13644e4ffcc9de70f1ee558099aee99b701
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-19 15:18:16 -0600

    Update TutorialScript.cs

--- CS350 Project 2/Assets/Scripts/TutorialScript.cs
+++ CS350 Project 2/Assets/Scripts/TutorialScript.cs
@@ -7,10 +7,16 @@ public class TutorialScript : MonoBehaviour
 
     [SerializeField] GameObject beginning;
     [SerializeField] GameObject worldSpace;
+    private int line = 1;
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0f;
+        foreach (Transform temp in beginning.transform)
+        {
+            temp.gameObject.SetActive(false);
+        }
+        beginning.transform.GetChild(line).gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -24,9 +30,18 @@ public class TutorialScript : MonoBehaviour
     {
         if(beginning.activeInHierarchy)
         {
-            Time.timeScale = 1f;
-            worldSpace.SetActive(true);
-            beginning.SetActive(false);
+            line++;
+            if (line >= beginning.transform.childCount)
+            {
+                Time.timeScale = 1f;
+                worldSpace.SetActive(true);
+                beginning.SetActive(false);
+            }
+            else
+            {
+                beginning.transform.GetChild(line).gameObject.SetActive(true);
+                beginning.transform.GetChild(line - 1).gameObject.SetActive(false);
+            }
         }
     }
 }
